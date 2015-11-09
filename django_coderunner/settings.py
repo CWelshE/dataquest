8fc58abbcdcd87070ba8dadb8606bc39edc809f9
Cody Welsh <codyw@protonmail.com>
2015-11-08 16:26:39 -0800

    change to Posgres

--- django_coderunner/settings.py
+++ django_coderunner/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'django_coderunner.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'coderunner',
+        'USER': 'coder',
+        'PASSWORD': 'example',
+        'HOST': '127.0.0.1',
+        'PORT': '8000',
     }
 }
 
