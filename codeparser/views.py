0f11fd5c454fc3dbc094e0250a2a1c1c991ef9fa
Cody Welsh <codyw@protonmail.com>
2015-11-13 23:53:24 -0800

    view framework

--- codeparser/views.py
+++ codeparser/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response
+from django.http import HttpResponse
+from django.template.context_processors import csrf
+from django.template import loader
 
-# Create your views here.
+import json
+import subprocess
+
+def index(request):
+    c = {}
+    c.update(csrf(request))
+    template = loader.get_template('codeparser/index.html')
+    return render_to_response("codeparser/index.html", c)
+
+def get_code(request):
+    code_txt = request.POST.get('code')
+    response_data = {}
+    response_data['code'] = code_txt
+
+    subprocess.call(["ls", "-l"])
+
+    return HttpResponse(
+            json.dumps(response_data),
+            content_type = "application/json"
+            )
+        
